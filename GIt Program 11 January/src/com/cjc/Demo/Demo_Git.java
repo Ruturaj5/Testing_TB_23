8df361631b175efb847d4a35643f852cd11b4504
rutur <rutur@RUTURAJ>
2024-01-11 11:55:29 +0530

    By Ruturaj

--- GIt Program 11 January/src/com/cjc/Demo/Demo_Git.java
+++ GIt Program 11 January/src/com/cjc/Demo/Demo_Git.java
@@ -3,10 +3,50 @@ package com.cjc.Demo;
 public class Demo_Git 
 {
 
+	
+	public void m1() 
+	{
+		System.out.println("Hello Git M1 Check");
+	}
+	
+	public void m2() 
+	{
+		System.out.println("Hello Git M2 Check");	
+	}
+	
+	
+	public void m3() 
+	{
+		System.out.println("Hello Git M3 Check");
+	}
+	
+	
+	public void m4() 
+	{
+		System.out.println("Hello Git M4 Check");
+	}
+	
+	
+	public void m5() 
+	{
+		System.out.println("Hello Git M5 Check");
+	}
+	
+	
+	
+	
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello Git Check");
-
+		Demo_Git  dg = new Demo_Git ();
+		dg.m1();
+		dg.m2();
+		dg.m3();
+		dg.m4();
+		dg.m5();
+	
+	
+	
 	}
 
 }
